42f81a41afc42a01cc9614255b701de40adc7423
Matthew Canan <matthew.canan@gmail.com>
2020-08-20 17:48:36 -0400

    further refinements, added sort function, pre-build and error checking

--- bggapi.py
+++ bggapi.py
@@ -8,11 +8,12 @@ class Collection:
         self.expansions = []
         self.wish_list = []
 
-    def __build_dict(self, p, fp):
+    @staticmethod
+    def __build_dict(p, fp):
         rank = fp.stats.rating.ranks.rank
         rank_list = []
 
-        # Handling untangled returning single-element list containing string in dictionary format for ranks
+        # Handling untangle returning single-element list containing string in dictionary format for ranks
         for i in range(len(rank)):
             temp = str(rank[i])
             arr = temp.split(sep="'")
@@ -49,11 +50,30 @@ class Collection:
 
         return _d
 
+
+    def __pre_build(self, _obj, _full_obj, _exp):
+        for i in range(len(_obj.items)):
+            _path = _obj.items.item[i]
+            _full_path = _full_obj.items.item[i]
+            _game_dict = self.__build_dict(_path, _full_path)
+
+            if int(_game_dict['own']):
+                if _exp:
+                    self.expansions.append(_game_dict)
+                else:
+                    self.games.append(_game_dict)
+            elif int(_game_dict['wish_list']):
+                self.wish_list.append(_game_dict)
+            else:
+                pass
+
     def load(self):
         check = True
         no_expansion = "&excludessubtype=boardgameexpansion"
         expansion = "&subtype=boardgameexpansion"
         full_stats = "&stats=1"
+
+        # Three calls are necessary due to quirks in boardgamegeek.com's API - see bgg xml document tree.txt
 
         while check:
             api_url = str("https://api.geekdo.com/xmlapi2/collection?username=" + self.name)
@@ -68,34 +88,11 @@ class Collection:
             except AttributeError:
                 pass
 
+        self.__pre_build(obj_games, obj_full, 0)
+        self.__pre_build(obj_expansion, obj_full, 1)
 
-
-        # TODO: combine game and expansion iteration into one function
-        for i in range(len(obj_games.items)):
-            path = obj_games.items.item[i]
-            full_path = obj_full.items.item[i]
-            game_dict = self.__build_dict(path, full_path)
-
-            if int(game_dict['own']):
-                self.games.append(game_dict)
-            elif int(game_dict['wish_list']):
-                self.wish_list.append(game_dict)
-            else:
-                pass
-
-        for i in range(len(obj_expansion.items)):
-            e_path = obj_expansion.items.item[i]
-            e_full_path = obj_full.items.item[i]
-            game_dict = self.__build_dict(e_path, e_full_path)
-
-            if int(game_dict['own']):
-                self.expansions.append(game_dict)
-            elif int(game_dict['wish_list']):
-                self.wish_list.append[game_dict]
-            else:
-                pass
-
-    def out(self, subset):
+    def out_formatted(self, subset):
+        count = 0
 
         while not (subset.lower() in ['games', 'wish_list', 'expansions']):
             subset = input("Usage -- 'games' 'wish_list' 'expansions': ")
@@ -110,45 +107,28 @@ class Collection:
         for i in range(len(subset)):
             for keys in subset[i]:
                 if type(subset[i][keys]) == list:
-                    l = len(subset[i][keys])
-                    for j in range(0, l, 2):
+                    for j in range(0, len(subset[i][keys]), 2):
                         print(f'{subset[i][keys][j]}: {subset[i][keys][j+1]}')
                 else:
                     print(f'{keys}: {subset[i][keys]}')
             print("-" * 20)
+            count = count + 1
+        print(f'Total:  {count}')
 
-'''TODO def to_json():'''
+    def sort_by(self, sort_type):
+        #while sort_type not in []
+        self.games = sorted(self.games, key=lambda game: int(game['num_plays']))
 
 
+
+#TODO def to_json():
+#jstr = json.dumps(x.games)
+#
 
 name = input("Enter User Name: ")
 x = Collection(name)
 x.load()
 option = input ("Type 'games' to print owned games, 'wish_list' for wishlist, 'expansions' for expansions:  ")
-x.out(option)
-jstr = json.dumps(x.games)
-print(jstr)
+x.sort_by('get_num_plays')
+x.out_formatted(option)
 
-
-'''The following didn't work:
-In load function...
-
-        self.__pre_build(obj_games, 0)
-        self.__pre_build(obj_expansion, 1)
-
-
-    def __pre_build(self, _obj, _exp):
-        for i in range(len(_obj.items)):
-            _path = _obj.items.item[i]
-            _full_path = _obj.items.item[i]
-            _game_dict = self.__build_dict(_path, _full_path)
-
-            if int(_game_dict['own']):
-                if _exp:
-                    self.expansions.append(_game_dict)
-                else:
-                    self.games.append(_game_dict)
-            elif int(_game_dict['wish_list']):
-                self.wish_list.append(_game_dict)
-            else:
-                pass'''
