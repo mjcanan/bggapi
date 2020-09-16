d71f0d3d9ae8dc96c2c1bc9d2eb6381cec27f53e
Matthew Canan <matthew.canan@gmail.com>
2020-09-16 17:50:35 -0400

    trying to figure out tkinter

--- bggapiGUI.py
+++ bggapiGUI.py
@@ -1,16 +1,11 @@
 from tkinter import *
 from bggapi import *
 
-root = Tk()
-u_name = Entry(root)
-u_name.grid(row=0,column=0)
-global options
-options = []
-
-
+'''CURRENTLY CAUSING 429 RESPONSE'''
 def callback(selection):
     labels = []
     j = 0
+
     for i in range(len(c.games)):
         if c.games[i]['name'] == selection:
             g_index = i
@@ -27,7 +22,20 @@ def load_click():
     user_name = u_name.get()
     global c
     c = Collection(user_name)
-    c.load()
+
+    if c.load() == 1:
+        err_label = Label(root, text="Invalid User")
+        err_label.grid(row=2, column=0)
+        return
+    elif c.load() == 2:
+        err_label = Label(root, text="Response 202: Retrying in 3 seconds")
+        err_label.grid(row=2, column=0)
+        return
+    elif c.load() == 3:
+        err_label = Label(root, text="User has no available data")
+        err_label.grid(row=2, column=0)
+        return
+
     #c.load_price()
     # c.load_price(c.wish_list)
     # c.load_price(c.expansions)
@@ -39,6 +47,16 @@ def load_click():
     ugh.set(options[0])
     opt = OptionMenu(root, ugh, *options,command=callback)
     opt.grid(row=2,column=0)
+    load_button.destroy()
+    u_name.destroy()
+
+
+
+
+root = Tk()
+u_name = Entry(root)
+u_name.grid(row=0,column=0)
+options = []
 
 
 load_button = Button(root,text="Enter User Name", command=load_click)
